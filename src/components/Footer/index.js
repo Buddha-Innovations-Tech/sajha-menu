9ae7065f15961afef7f45141beeb1d39491b707a
Saroj Kunwar <saroj@tunatechnology.com>
2023-05-16 10:22:31 +0545

    contacts updated

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -62,7 +62,7 @@ const Footer = () => {
               <ul className='clearfix footer__blocks--content'>
                 <li className='mb-20'>
                   <FaPhoneAlt className='icon mr-13' />
-                  071438471
+                  071-438471, 9857043895, 9857043891
                 </li>
                 <li className='mb-20'>
                   <FaEnvelope className='icon mr-13' />
